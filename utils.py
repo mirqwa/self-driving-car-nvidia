191b38424eba6179c6e18a487730da1169dc6253
mirqwa <kimrodrikwa@gmail.com>
2023-08-10 07:34:51 +0300

    extract rebalancing to a function

--- utils.py
+++ utils.py
@@ -9,6 +9,14 @@ def get_sample_size(x):
     counts = len(x)
     return counts * 2 if counts < 1000 else counts
 
+def rebalance_data(df):
+    df = (
+        df.groupby("steering")
+        .apply(lambda x: x.sample(n=get_sample_size(x), replace=True))
+        .reset_index(drop=True)
+    )
+    return df
+
 
 def load_data(data_dir, test_size):
     """
@@ -18,11 +26,7 @@ def load_data(data_dir, test_size):
         os.path.join(os.getcwd(), data_dir, "driving_log.csv"),
         names=["center", "left", "right", "steering", "throttle", "reverse", "speed"],
     )
-    df = (
-        df.groupby("steering")
-        .apply(lambda x: x.sample(n=get_sample_size(x), replace=True))
-        .reset_index(drop=True)
-    )
+    df = rebalance_data(df)
     X = df[["center", "left", "right"]].values
     Y = df["steering"].values
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size)
