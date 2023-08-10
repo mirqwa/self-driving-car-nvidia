3967a4264d782fab85032b998461ac2ce646c060
mirqwa <kimrodrikwa@gmail.com>
2023-08-10 07:32:41 +0300

    get expected columns

--- utils.py
+++ utils.py
@@ -15,13 +15,16 @@ def load_data(data_dir, test_size):
     load simulator driving training data
     """
     df = pd.read_csv(
-        os.path.join(os.getcwd(), data_dir, 'driving_log.csv'),
+        os.path.join(os.getcwd(), data_dir, "driving_log.csv"),
+        names=["center", "left", "right", "steering", "throttle", "reverse", "speed"],
     )
-    print(df.columns.to_list())
-    df = df.groupby('steering').apply(
-        lambda x: x.sample(n=get_sample_size(x), replace=True)).reset_index(drop=True)
-    X = df[['center', 'left', 'right']].values
-    Y = df['steering'].values
+    df = (
+        df.groupby("steering")
+        .apply(lambda x: x.sample(n=get_sample_size(x), replace=True))
+        .reset_index(drop=True)
+    )
+    X = df[["center", "left", "right"]].values
+    Y = df["steering"].values
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size)
     X_train, Y_train = shuffle(X_train, Y_train)
     X_test, Y_test = shuffle(X_test, Y_test)
